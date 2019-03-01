5fe6b04bb069bde0f54077bc83508c1868214319
wzlUp <1076500039@qq.com>
2019-03-01 15:41:00 +0800

    屏蔽target文件上传

--- micro-project-parent/micro-project-service/src/main/java/com/dalei/freedom/service/impl/UserServiceImpl.java
+++ micro-project-parent/micro-project-service/src/main/java/com/dalei/freedom/service/impl/UserServiceImpl.java
@@ -30,6 +30,7 @@ public class UserServiceImpl implements UserService {
     public String getNowDateTime() {
         String nowDate = DateToolUtil.getNowDate();
         System.out.println("当前时间：" + nowDate);
+        System.out.println("当前时间：" + nowDate);
         return nowDate;
     }
 }
